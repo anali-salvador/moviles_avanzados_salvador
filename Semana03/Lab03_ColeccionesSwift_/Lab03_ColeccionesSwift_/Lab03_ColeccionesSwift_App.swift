1424a4199f267cda63af09cbb6bb536c58a2a123
anali salvador <anali.salvador@tecsup.edu.pe>
2026-09-03 13:45:47 -0500

    feat(arrays): implementar manipulacion de arreglos para estaciones del Metro de Lima

---
+++ Semana03/Lab03_ColeccionesSwift_/Lab03_ColeccionesSwift_/Lab03_ColeccionesSwift_App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab03_ColeccionesSwift_App.swift
+//  Lab03_ColeccionesSwift_
+//
+//  Created by Tecsup on 3/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Lab03_ColeccionesSwift_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
