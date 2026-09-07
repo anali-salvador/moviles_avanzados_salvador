0f2e44862f12ff9cc4c6ec66416370e49e5866ed
anali salvador <anali.salvador@tecsup.edu.pe>
2026-09-07 17:45:41 -0500

    RF05: menu principal en bucle con validacion de entradas

---
+++ Semana03/Metropolitano/Metropolitano/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Metropolitano
+//
+//  Created by Tecsup on 7/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Semana03/Metropolitano/Metropolitano/MetropolitanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetropolitanoApp.swift
+//  Metropolitano
+//
+//  Created by Tecsup on 7/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MetropolitanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
